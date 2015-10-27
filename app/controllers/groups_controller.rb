6e69619026b1ef89e6fbc887acd06a7022d7f0b0
nigelfloe <dwr229@gmail.com>
2015-10-27 13:36:58 -0400

    group creation in progress

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,24 @@
+class GroupsController < ApplicationController
+  def index
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
